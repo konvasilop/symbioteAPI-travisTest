6602b89f99e98d2505a6824437b0115ed8891f42
Vasilopoulos Konstantinos <konvasilop@intracom-telecom.com>
2021-09-02 13:33:01 +0300

    showing CI to ilia

---
+++ pushing_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+: "${GITHUB_ACCESS_TOKEN?}" "${GITHUB_REPO?}"
+
+## shellcheck disable=SC2164
+
+push_uri="https://GITHUB_ACCESS_TOKEN@github.com/$GITHUB_REPO"
+
+# Redirect to /dev/null to avoid secret leakage
+printf 'git push %s staging:develop\n' "$GITHUB_REPO"
+git push "$push_uri" staging:develop
