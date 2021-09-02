c8e02aeb6d593e621241c8ca03cea3857b29a21b
Vasilopoulos Konstantinos <konvasilop@intracom-telecom.com>
2021-09-02 16:24:00 +0300

    auto merging and pushing to dockerhub

--- docker_build.sh
+++ docker_build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -e
 
 : "${DOCKER_IMAGE_NAME?}"
 
 printf 'build' "$DOCKER_IMAGE_NAME"
-docker build --build-arg JAR_FILE=build/libs/\*.jar  -t $(DOCKER_IMAGE_NAME) .
-docker run -d -p 9191:9191 -p 8443:8443 $(DOCKER_IMAGE_NAME)
+docker build --build-arg JAR_FILE=build/libs/\*.jar  -t $DOCKER_IMAGE_NAME .
+docker run -d -p 9191:9191 -p 8443:8443 $DOCKER_IMAGE_NAME
 docker ps -a
--- docker_push.sh
+++ docker_push.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -e
 
 : "${DOCKER_PASSWORD?}" "${DOCKER_USERNAME?}" "${DOCKER_IMAGE_NAME?}"
 
-echo "$(DOCKER_PASSWORD)" | docker login -u "$(DOCKER_USERNAME)" --password-stdin
-docker tag $(DOCKER_IMAGE_NAME) symbiote-h2020/$(DOCKER_IMAGE_NAME):latest
-docker push symbiote-h2020/$(DOCKER_IMAGE_NAME):latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker tag $DOCKER_IMAGE_NAME symbiote-h2020/$DOCKER_IMAGE_NAME:latest
+docker push symbiote-h2020/$DOCKER_IMAGE_NAME:latest
