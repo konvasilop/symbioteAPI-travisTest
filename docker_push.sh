8736ee607aa9a08bb9dfc099cecd1fc8a084b21d
Vasilopoulos Konstantinos <konvasilop@intracom-telecom.com>
2021-09-03 09:50:45 +0300

    auto merging and pushing to dockerhub

--- docker_push.sh
+++ docker_push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-:"${$DOCKER_IMAGE_NAME?}" "${DOCKER_USERNAME?}" "${DOCKER_PASSWORD?}"
+:"${DOCKER_IMAGE_NAME?}" "${DOCKER_USERNAME?}" "${DOCKER_PASSWORD?}"
+
+printf "$DOCKER_IMAGE_NAME"
+printf "$DOCKER_USERNAME"
 
 printf 'About to push ' "$DOCKER_IMAGE_NAME" ' with ' "$DOCKER_USERNAME"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
