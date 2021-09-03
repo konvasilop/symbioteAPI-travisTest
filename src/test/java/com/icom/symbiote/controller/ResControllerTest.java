41f8aa03ca3d553e12ea13d4c2f6855d8a6682f2
Vasilopoulos Konstantinos <konvasilop@intracom-telecom.com>
2021-09-03 14:33:08 +0300

    run tests on push

---
+++ src/test/java/com/icom/symbiote/controller/ResControllerTest.java
@@ -0,0 +1,14 @@
+package com.icom.symbiote.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResControllerTest {
+
+    @Test
+    void postGetL2Res() {
+        assertTrue(true);
+    }
+}
