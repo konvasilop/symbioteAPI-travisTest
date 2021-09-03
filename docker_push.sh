039aeed85ed49e3a48a1905feec1eecd582003de
Vasilopoulos Konstantinos <konvasilop@intracom-telecom.com>
2021-09-03 09:49:09 +0300

    auto merging and pushing to dockerhub

--- docker_push.sh
+++ docker_push.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-: "${DOCKER_IMAGE_NAME?}" "${DOCKER_USERNAME?}" "${DOCKER_PASSWORD?}"
+:"${$DOCKER_IMAGE_NAME?}" "${DOCKER_USERNAME?}" "${DOCKER_PASSWORD?}"
 
-printf 'about to push ' "$DOCKER_IMAGE_NAME" ' with ' "$DOCKER_USERNAME"
+printf 'About to push ' "$DOCKER_IMAGE_NAME" ' with ' "$DOCKER_USERNAME"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker tag $DOCKER_IMAGE_NAME symbiote-h2020/$DOCKER_IMAGE_NAME:latest
-docker push symbiote-h2020/$DOCKER_IMAGE_NAME:latest
+docker push symbiote-h2020/"$DOCKER_IMAGE_NAME":latest
